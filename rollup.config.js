15d150cac8ad7badd39151435001b0dbf8441be4
darrendahl <darrencdahl@gmail.com>
2018-03-07 09:32:09 -0800

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from "rollup-plugin-babel";
 
 export default {
   entry: "src/index.js",
-  targets: [{ dest: "dist/bundle.js", format: "es" }],
+  targets: [{ dest: "dist/bundle.js", format: "cjs" }],
   plugins: [
     babel({
       exclude: ["node_modules/**"],
